9583697375f64bd1ef2d7e8bbeb38968ce141e1c
Giulia Sellitto <giuliasellitto.gs@gmail.com>
2022-09-27 18:09:43 +0200

    Upload project

---
+++ src/test/java/it/unisa/gisellitto/MainTest.java
@@ -0,0 +1,13 @@
+package it.unisa.gisellitto;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+
+    @Test
+    void demoTestMethod() {
+        assertTrue(true);
+    }
+}
